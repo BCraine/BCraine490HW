b4f996d8152bf23f779b0201e015301b8ed07e4f
BCraine <brandoncraine193@gmail.com>
2021-04-25 13:11:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def main():
+    i = 45
+    b = 32
+    j = b+i
+    return j
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ tests/test_490HW.py
@@ -0,0 +1,6 @@
+import main
+
+
+def test_happy():
+    result = main.main()
+    assert result == 77
